122e71cd95422dde8a1131de7827eaf22e6bd0e8
khalid chouhan <khalidchuhan7886@gmail.com>
2024-11-11 23:45:07 +0500

    image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <img
-        src="/Assets/images/customer-service-in-banking--jpg.webp"
+        src="/public/Assets/images/customer-service-in-banking--jpg.webp"
         alt="image"
         className="w-full p-0 max-w-[100%] h-[80vh]  relative object-cover"
       />
